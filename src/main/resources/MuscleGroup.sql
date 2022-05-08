9f57a70b695ae1f55e56dd5313b68d6cf320904d
zabkins <mzarczynski29@gmail.com>
2022-05-08 21:56:08 +0200

    MuscleGroup.sql added

---
+++ src/main/resources/MuscleGroup.sql
@@ -0,0 +1,8 @@
+INSERT INTO muscle_group (name) VALUES ('Back');
+INSERT INTO muscle_group (name) VALUES ('Biceps');
+INSERT INTO muscle_group (name) VALUES ('Chest');
+INSERT INTO muscle_group (name) VALUES ('Core');
+INSERT INTO muscle_group (name) VALUES ('Forearms');
+INSERT INTO muscle_group (name) VALUES ('Legs');
+INSERT INTO muscle_group (name) VALUES ('Shoulders');
+INSERT INTO muscle_group (name) VALUES ('Triceps');
